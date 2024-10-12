0478f5fd3459ead48d85aa2172f72319793977c9
natika1 <natika1@g.pl>
2024-10-12 20:07:41 +0200

    Added string in polish and it works fine

--- integrate.py
+++ integrate.py
@@ -107,3 +107,5 @@ if __name__ == "__main__":
             #print(docContain)
             text_to_speech_file("Witaj świecie, może razem uda się ulepszyć Polską platformę edukacyjną!")
             #text_to_speech_file([posts_string])
+
+            
