be9658ab04eec4ee87d3f77d887348fcca7d8d31
natika1 <natika1@g.pl>
2024-10-12 17:50:06 +0200

    working api call to ElevenLabs

--- integrate.py
+++ integrate.py
@@ -1,22 +1,41 @@
-
 import os
 import uuid
+
+from dotenv import load_dotenv
 from elevenlabs import VoiceSettings
 from elevenlabs.client import ElevenLabs
 
-ELEVENLABS_API_KEY = os.getenv("11KEY") #sk_f55dae6021ac30c7e36b2ef2e48c7101902f6930836ac947
+load_dotenv()
+
+ELEVENLABS_API_KEY = "sk_f55dae6021ac30c7e36b2ef2e48c7101902f6930836ac947"
+
+if not ELEVENLABS_API_KEY:
+    raise ValueError("ELEVENLABS_API_KEY environment variable not set")
+
 client = ElevenLabs(
     api_key=ELEVENLABS_API_KEY,
 )
-
-
 def text_to_speech_file(text: str) -> str:
+    """
+    Converts text to speech and saves the output as an MP3 file.
+
+    This function uses a specific client for text-to-speech conversion. It configures
+    various parameters for the voice output and saves the resulting audio stream to an
+    MP3 file with a unique name.
+
+    Args:
+        text (str): The text content to convert to speech.
+
+    Returns:
+        str: The file path where the audio file has been saved.
+    """
     # Calling the text_to_speech conversion API with detailed parameters
     response = client.text_to_speech.convert(
-        voice_id="pNInz6obpgDQGcFmaJgB", # Adam pre-made voice
+        voice_id="pNInz6obpgDQGcFmaJgB",  # Adam pre-made voice
+        optimize_streaming_latency="0",
         output_format="mp3_22050_32",
         text=text,
-        model_id="eleven_turbo_v2_5", # use the turbo model for low latency
+        model_id="eleven_turbo_v2",  # use the turbo model for low latency, for other languages use the `eleven_multilingual_v2`
         voice_settings=VoiceSettings(
             stability=0.0,
             similarity_boost=1.0,
@@ -25,20 +44,20 @@ def text_to_speech_file(text: str) -> str:
         ),
     )
 
-    # uncomment the line below to play the audio back
-    # play(response)
-
     # Generating a unique file name for the output MP3 file
     save_file_path = f"{uuid.uuid4()}.mp3"
+    # Writing the audio stream to the file
 
-    # Writing the audio to a file
     with open(save_file_path, "wb") as f:
         for chunk in response:
             if chunk:
                 f.write(chunk)
 
-    print(f"{save_file_path}: A new audio file was saved successfully!")
+    print(f"A new audio file was saved successfully at {save_file_path}")
 
     # Return the path of the saved audio file
     return save_file_path
 
+
+if __name__ == "__main__":
+    text_to_speech_file("Witaj świecie!! To mój pierwszy raz kiedy mówię po polsku.")
